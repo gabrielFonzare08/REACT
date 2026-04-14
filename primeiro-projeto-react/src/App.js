55dfd80ddc4b864f0925dddd553d56e5e376f6b8
Gabriel Fonzare <gabrielfonzare@gmail.com>
2026-04-14 19:49:54 -0300

    Mudanças de estado - COmponente

--- primeiro-projeto-react/src/App.js
+++ primeiro-projeto-react/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    nome: ''
+  }
+
+  modificarNome = (event) => {
+      let nome = event.target.value;
+      this.setState({
+        nome : nome
+      })
+  }
+
+    render(){
+        return (
+          <React.Fragment>
+            <input type="text" value={this.state.nome} onChange={ this.modificarNome}></input>
+          <h1>Hello from APP {this.state.nome} </h1>
+          </React.Fragment>
+        )
+    }
 }
 
 export default App;
